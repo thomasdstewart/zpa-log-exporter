2b55732bf331565cebe64d6c1430ee8577284a78
Thomas Stewart <thomas@stewarts.org.uk>
2025-12-17 03:27:41 +0000

    chore: add licence and update description

--- zpa_exporter.py
+++ zpa_exporter.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 """
-Zscaler ZPA App Connector Prometheus exporter.
+Copyright (C) 2025 Thomas Stewart <thomas@stewarts.org.uk>
 
-- Tails journald (journalctl -f) for zpa-connector-child messages.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Zscaler ZPA App Connector Prometheus exporter.
+- Tails journald for zpa-connector-child messages.
 - Parses Mtunnels(...) metrics lines.
-- Exposes metrics via /metrics on port 8080 in Prometheus format **or** writes
-  a
+- Exposes metrics via /metrics on port 8080 **or** writes a
   Prometheus textfile collector `.prom` file for consumption by the Node
   Exporter.
-
-Requires:
-    pip install prometheus_client
-
-Run:
-    python zpa_exporter.py
 """
 
 import os
